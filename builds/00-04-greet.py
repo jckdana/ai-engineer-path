6550c33420e2230793e5048e86db6c99843a1f25
jckdana <JMDanache@gmail.com>
2026-08-19 17:10:11 -0700

    Added greeding script from lesson 00-04

---
+++ builds/00-04-greet.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ").strip().title()
+lessons_done = int(input("How many lessons have you completed? "))
+
+print(f"Hello {name}, you have completed {lessons_done} lessons!")
+
